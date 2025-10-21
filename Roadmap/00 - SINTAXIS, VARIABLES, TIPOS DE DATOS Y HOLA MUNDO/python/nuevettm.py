73c2489a6ebc9545dd07088bd72537c15ede4882
nuevettm <axelh6182@gmail.com>
2025-10-20 19:35:16 -0600

    nuevettm.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/nuevettm.py
@@ -0,0 +1,26 @@
+#https://python.org
+#comentario en una linea 
+
+"""
+esto creo tambien es un comentario, espero estar haciendo 
+las cosa sbien pq no entiendo nd
+"""
+
+'''
+esto tambien es un comentario
+'''
+
+my_variable = "mi variable"
+my_variable = "nuevo valor de mi vcariable"
+
+MY_CONSTANT = "mi constabnte"
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool= False
+my_string = "mi cadena de texto"
+
+
+print ("hola, phyton")
+
